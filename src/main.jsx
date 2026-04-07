c408fb50d2e7ca6376c25c5d5477d3f07c704f19
Wesley Geilson <contatogeilsonwg@gmail.com>
2026-04-07 11:38:59 -0300

    chore: renomeamento de pasta

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import HomeBrowser from './Pages/.jsx' /* importe pasta de documento pai -> documento -> filho */
+import HomeBrowser from './Pages/HomeBrowser' /* importe pasta de documento pai -> documento -> filho */
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
